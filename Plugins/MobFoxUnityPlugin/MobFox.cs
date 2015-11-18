07b94cd284ce4fbff1e16ab5e0908edcba64d32b
Itamar Nabriski <itamar.n@matomy.com>
2015-11-18 15:58:31 +0200

    added c# file

---
+++ Plugins/MobFoxUnityPlugin/MobFox.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class MobFox : MonoBehaviour
+{
+
+	//public delegate void InterstitialReadyDelegate();
+	//public InterstitialReadyDelegate interstitialReadyDelegate;
+
+	[DllImport("__Internal")]
+	public static extern void _setGameObject(string gameObject);
+
+	[DllImport("__Internal")]
+	public static extern int _createBanner(string invh);
+
+	[DllImport("__Internal")]
+	public static extern void _showBanner (int bannerId);
+
+	[DllImport("__Internal")]
+	public static extern void  _hideBanner (int bannerId);
+
+	[DllImport("__Internal")]
+	public static extern void _createInterstitial(string invh);
+
+	[DllImport("__Internal")]
+	public static extern void  _showInterstitial();
+
+	//public static void showInterstital(int interId){
+	//	_showInterstitial(interId);
+	//}
+
+	public static void setGameObject(string gameObj){
+		_setGameObject(gameObj);
+	}
+	
+	//banners
+	public static int createBanner(string invh){
+		return _createBanner(invh);
+	}
+
+	public static void showBanner (int bannerId){
+		_showBanner (bannerId);
+	}
+
+	public static void  hideBanner (int bannerId){
+		_hideBanner (bannerId);
+	}
+
+	//interstitial
+	public static void showInterstitial(){
+		_showInterstitial();
+	}
+
+	public static void createInterstitial(string invh){
+		_createInterstitial(invh);
+	}
+
+	
+	
+}
