97822ba2c5fa73174be0d525d4346ca2c4183436
Shimi Sheetrit <shimi.s@matomy.com>
2016-08-15 15:00:39 +0300

    AdMobAdapterSample project added

---
+++ AdMobAdapterSample/AdMobAdapterSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdMobAdapterSample
+//
+//  Created by Shimi Sheetrit on 8/15/16.
+//  Copyright © 2016 Matomy Media Group Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
